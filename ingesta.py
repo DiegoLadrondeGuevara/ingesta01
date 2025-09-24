82a4a86a576a486354b859e076e24d684782ae06
DiegoLadrondeGuevara <diego.ladrondeguevar@utec.edu.pe>
2025-09-24 07:29:31 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,10 +1,11 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "dladron-s3"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(ficheroUpload, nombreBucket, "ingesta/" + ficheroUpload)
 print(response)
+
 
 print("Ingesta completada")
